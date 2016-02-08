6480bd16e07260c09d88379e45ecc639110bb0ba
James W. Barnett <jbarnet4@tulane.edu>
2016-02-08 10:46:32 -0600

    move pymbar version check in setup.py

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ setup(
     # your project is installed. For an analysis of "install_requires" vs pip's
     # requirements files see:
     # https://packaging.python.org/en/latest/requirements.html
-    install_requires=["pymbar"],
+    install_requires=["pymbar>=3.0.0.dev0"],
 
     # List additional groups of dependencies here (e.g. development
     # dependencies). You can install these using the following syntax,
