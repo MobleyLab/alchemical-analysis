994b471dbb03a16109238bc810ae946e1ede8caf
Nathan <limn1@uci.edu>
2016-02-18 12:22:38 -0800

    Cleaned up unnecessary lines

--- alchemical_analysis/zeroxvg.py
+++ alchemical_analysis/zeroxvg.py
@@ -1,6 +1,7 @@
 import numpy,os,sys
 
 def zero_output(K,P):
+   """Adapted from totalEnergies() function to write out all zeros in results.txt file"""
    df_allk = list(); ddf_allk = list()
    for k in range(K-1):
       df=dict(); ddf=dict()
@@ -11,20 +12,14 @@ def zero_output(K,P):
       ddf_allk = numpy.append(ddf_allk,ddf)
    segments      = ['Coulomb'  , 'vdWaals'  , 'TOTAL']
    def printLine(str1, str2, d1=None, d2=None):
-      """Fills out the results table linewise."""
-      #print str1,
       text = str1
       for name in P.methods:
          if d1 == 'plain':
-            #print str2,
             text += ' ' + str2
          if d1 == 'name':
-            #print str2 % (name, P.units),
             text += ' ' + str2 % (name, P.units)
          if d1 and d2:
-            #print str2 % (d1[name]/P.beta_report, d2[name]/P.beta_report),
             text += ' ' + str2 % (d1[name]/P.beta_report, d2[name]/P.beta_report)
-      #print ''
       outtext.append(text + '\n')
       return
 
